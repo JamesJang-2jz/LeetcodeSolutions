e6ac4d6bbe69330013937b02da015d3aa8774861
JamesJang-2jz <jhjx92@gmail.com>
2023-08-15 15:41:53 -0400

    finished Roman to Int

--- src/main/java/RomanToIntLC.java
+++ src/main/java/RomanToIntLC.java
@@ -1,4 +1,5 @@
-package org.example;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RomanToIntLC {
 
@@ -19,6 +20,26 @@ public class RomanToIntLC {
                 result -= num;
             } else {
                 result += num;
+            }
+        }
+        return result;
+    }
+
+    public int romanToInt(String s, String iterateFrom0){
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
             }
         }
         return result;
