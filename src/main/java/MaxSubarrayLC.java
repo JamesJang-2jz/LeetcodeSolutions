a8bd980ffe1622ced0fb0be225105bb7d1c7cabf
JamesJang-2jz <jhjx92@gmail.com>
2023-08-16 13:23:18 -0400

    finished max subarray

--- src/main/java/MaxSubarrayLC.java
+++ src/main/java/MaxSubarrayLC.java
@@ -15,4 +15,15 @@ public class MaxSubarrayLC {
         }
         return max;
     }
+
+    public int maxSubArray(int[] nums, String maxFunction){
+        int max = Integer.MIN_VALUE;
+        int current = 0;
+        int n  = nums.length;
+        for (int i = 0; i < n; i++) {
+            current = Math.max(nums[i], current + nums[i]);
+            max = Math.max(current, max);
+        }
+        return max;
+    }
 }
